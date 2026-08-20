77df64701129f6853eb952371f41a08843fec980
DMontgomery40 <dmontg@gmail.com>
2026-08-20 00:53:27 -0600

    refactor(ui): remove obsolete DevTools island

--- web/src/components/DevTools/index.ts
+++
@@ -1,8 +0,0 @@
-// TriBridRAG - DevTools Components Export
-// Exports all DevTools subtab components
-
-export { Editor } from './Editor';
-export { Debug } from './Debug';
-export { Testing } from './Testing';
-export { Reranker } from './Reranker';
-export { Integrations } from './Integrations';
